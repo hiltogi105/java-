035ece5f04ffd92a27a3775b03d651d42b35cd55
hilto <hilto@TOGY>
2026-02-18 06:32:18 +0900

    課題 java基礎問題_3

--- HelloWorld_TogoYuki/src/curriculum_B/Question3.java
+++ HelloWorld_TogoYuki/src/curriculum_B/Question3.java
@@ -1,5 +1,6 @@
 package curriculum_B;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Question3 {
@@ -8,42 +9,39 @@ public class Question3 {
 		// TODO 自動生成されたメソッド・スタブ
 		
 		//Q1
+		System.out.println(" Q1");
 		for(int i =1;i <=10;i++) {
 			System.out.println(i);
 		}
 		
-		//見やすくするための改行
-		System.out.println("");
+
 		
 		//Q2
+		System.out.println(" Q2");		
 		for(int i = 2;i <= 20;i++) {
 			if(i % 2 == 0) {
 				System.out.println(i);
 			}
 		}
 		
-		//見やすくするための改行
-		System.out.println("");
 		
 		//Q3
+		System.out.println(" Q3");
 		for(int i =10;i >=1;i--) {
 			System.out.println(i);
 		}
 		
-		//見やすくするための改行
-		System.out.println("");
-		
 		//Q4
+		System.out.println(" Q4");		
 		int sum = 0;
 		for(int i =1;i<=100;i++) {
 			sum += i;
 		}
 		System.out.println(sum);
 		
-		//見やすくするための改行
-		System.out.println("");		
 		
 		//Q5
+		System.out.println(" Q5");		
 		for(int i = 1;i <=5;i++) {
 			for(int j = 1;j <= i;j++) {
 				System.out.print("*");
@@ -52,20 +50,17 @@ public class Question3 {
 			
 		}
 		
-		//見やすくするための改行
-		System.out.println("");	
 		
 		//Q6
+		System.out.println(" Q6");		
 		int count = 1;
 		while(count <=10) {
 			System.out.println(count);
 			count++;
 		}
 		
-		//見やすくするための改行
-		System.out.println("");	
-		
 		//Q7
+		System.out.println(" Q7");	
 		count = 1;
 		while(count <=20) {
 			if(count % 2 == 0) {
@@ -73,21 +68,17 @@ public class Question3 {
 			}
 			count++;
 		}
-		
-		//見やすくするための改行
-		System.out.println("");	
 				
 		//Q8
+		System.out.println(" Q8");	
 		count = 10;
 		while(count >= 1) {
 			System.out.println(count);
 			count--;
 		}
-		
-		//見やすくするための改行
-		System.out.println("");	
 				
 		//Q9
+		System.out.println(" Q9");	
 		count = 1;
 		sum = 0;
 		while(count <=100) {
@@ -95,11 +86,9 @@ public class Question3 {
 			count++;
 		}
 		System.out.println(sum);
-		
-		//見やすくするための改行
-		System.out.println("");	
 						
 		//Q10
+		System.out.println(" Q10");			
 		Scanner sc = new Scanner(System.in);
 		
 		while(true) {
@@ -110,7 +99,7 @@ public class Question3 {
 					System.out.println("終了しました");
 					break;
 				}else {
-					//System.out.println(num);	
+					//System.out.println(num);
 				}
 			}else {
 				System.out.println("数字以外が入力されました");
@@ -118,12 +107,10 @@ public class Question3 {
 			}
 	
 		}
-		sc.close();
 		
-		//見やすくするための改行
-		System.out.println("");	
 		
 		//Q11
+		System.out.println(" Q11");			
 		int total;
 		for(int i = 1;i <=9;i++) {
 			for(int j = 1;j <=9;j++) {
@@ -138,6 +125,33 @@ public class Question3 {
 
 		}
 		
+		//Q12
+		System.out.println(" Q12");	
+		System.out.println("商品を入力してください");
+		Random rand = new Random();
+		sc.nextLine();
+		String input = sc.nextLine();
+		
+		
+		String[] items = input.split("、");
+		int tvStock = rand.nextInt(12); // 0~11
+		
+		for(String item:items) {
+			switch(item) {
+				case "テレビ":
+				case "ディスプレイ":
+					int stock = item.equals("テレビ")? tvStock:11 - tvStock;
+					System.out.println(item + "の残りの台数は" + stock + "です");
+					break;
+				case "パソコン":case "冷蔵庫":case "扇風機":case "洗濯機":case "加湿器":
+					int randomStock = rand.nextInt(12);
+					System.out.println(item + "の残りの台数は" + randomStock + "です");
+					break;
+				default:
+					System.out.println("『 " + item + " 』は指定の商品ではありません");
+			}
+		}
+		sc.close();
 	}
 
 }
