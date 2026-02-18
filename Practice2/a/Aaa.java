52d0de9f5d2167376f6f603436bfe4b9df7ca42c
hilto <hilto@TOGY>
2026-02-19 04:41:52 +0900

    a

---
+++ Practice2/a/Aaa.java
@@ -0,0 +1,17 @@
+package a;
+
+public class Aaa {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		1.public class Test {
+			2.　public static void main(String[] args) {
+			3.　　boolean b = true;
+			4.　　if(b = false)
+			5.　　System.out.println(b);
+			6.　　System.out.println(b);
+			7.　}
+			8.}
+	}
+
+}
